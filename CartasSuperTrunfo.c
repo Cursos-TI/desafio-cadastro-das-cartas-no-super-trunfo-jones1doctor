7660c3dbe2766db44518882b25a3457e9071c4b2
jones1doctor <31035858+jones1doctor@users.noreply.github.com>
2025-02-06 08:05:28 -0300

    Update CartasSuperTrunfo.c
    inclusão as primeiras propriedades
        População
        Área
        PIB
        Número de pontos turísticos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,6 +18,19 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    
+    /*
+    Criar cartas representando cidades, contendo as seguintes propriedades:
+    População
+    Área
+    PIB
+    Número de pontos turísticos
+
+        */
+    char carta[] = 'A01';
+    double populacao = 650000;
+    float area = 530253.4;
+    float pib = 15656545.84;
+    int pontosTuristicos = 5;
+    printf (
     return 0;
 }
